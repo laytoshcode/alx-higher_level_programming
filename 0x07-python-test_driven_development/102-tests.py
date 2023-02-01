f60b3844f80b4bff8bb957eab57e8768866b1880
laytoshcode <laytoshtwo@yahoo.com>
2023-02-01 20:57:45 +0100

    test

---
+++ 0x07-python-test_driven_development/102-tests.py
@@ -0,0 +1,20 @@
+ import ctypes
+
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_string.argtypes = [ctypes.py_object]
+s = "The spoon does not exist"
+lib.print_python_string(s)
+s = "ложка не существует"
+lib.print_python_string(s)
+s = "La cuillère n'existe pas"
+lib.print_python_string(s)
+s = "勺子不存在"
+lib.print_python_string(s)
+s = "숟가락은 존재하지 않는다."
+lib.print_python_string(s)
+s = "スプーンは存在しない"
+lib.print_python_string(s)
+s = b"The spoon does not exist"
+lib.print_python_string(s)
+julien@ubuntu:~/0x07. Pyhton Strings$ gcc -shared -Wl,-soname,libPython.so -o libPython.so -fPIC -I/usr/include/python3.4 102-python.c
+julien@ubuntu:~/0x07. Pyhton Strings$ python3 ./102-tests.py
